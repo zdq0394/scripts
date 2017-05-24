446807250e0e861b62d41ce1cc7b1d5d4d804a5f
zhangdongqi <zdq123.hn@163.com>
2017-05-24 21:44:09 +0800

    xf not zxf in tar command

--- test/unixbench/unixbench.sh
+++ test/unixbench/unixbench.sh
@@ -3,7 +3,7 @@ echo "========================================================================="
 echo "Thanks for using UnixBench 5.1.3 for CentOS Auto-Install Script"
 echo "========================================================================="
 yum -y install wget make gcc gcc-c automake autoconf libtool gcc-c++ time perl-Time-HiRes SDL-devel mesa-libGL-devel
-tar xzf UnixBench5.1.3.tar
+tar xf UnixBench5.1.3.tar
 cd UnixBench
 make
 ./Run
