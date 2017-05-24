e825a6a59f0b629704e81a57470c9fc8cb83c3d5
zhangdongqi <zdq123.hn@163.com>
2017-05-24 21:33:01 +0800

    remove git clone command

--- test/unixbench/unixbench.sh
+++ test/unixbench/unixbench.sh
@@ -3,17 +3,11 @@ echo "========================================================================="
 echo "Thanks for using UnixBench 5.1.3 for CentOS Auto-Install Script"
 echo "========================================================================="
 yum -y install wget make gcc gcc-c automake autoconf libtool gcc-c++ time perl-Time-HiRes SDL-devel mesa-libGL-devel
-git clone http://github.com/zdq0394/scripts.git
-cd scripts/test/unixbench
 tar xzf UnixBench5.1.3.tar
 cd UnixBench
 make
 ./Run
 echo "========================================================================="
 echo "test end"
-echo "========================================================================="
-echo ""
-echo "http://bbs.itzmx.com"
-echo ""
 echo "Thank you!"
 echo "========================================================================="
