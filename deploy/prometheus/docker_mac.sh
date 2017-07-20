e148d283ce8a4b8dc419c911f44a83d45dd4696a
zhangdongqi <zhangdongqi@qiniu.com>
2017-07-20 13:41:17 +0800

    add os check for system

--- deploy/prometheus/docker_mac.sh
+++
@@ -1,2 +0,0 @@
-sysctl -w net.inet.ip.forwarding=1
-docker run -d --name prometheus -p 9090:9090 prom/prometheus
---
+++ system/os_check.sh
@@ -0,0 +1,8 @@
+os=`uname -s`
+if test $os = "Darwin" ; then
+    echo "Darwin"
+fi
+
+if test $os = "Linux" ; then
+    echo "Linux"
+fi
