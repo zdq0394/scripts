de49ba7e34141713599110a52d29d898a85c5297
zhangdongqi <zhangdongqi@qiniu.com>
2017-07-20 16:34:15 +0800

    add alertmanager deploy

---
+++ deploy/alertmanager/docker.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+os=`uname -s`
+if test $os = "Darwin" ; then
+    echo "Darwin"
+    sysctl -w net.inet.ip.forwarding=1
+fi
+
+if test $os = "Linux" ; then
+    echo "Linux"
+    echo "1" > /proc/sys/net/ipv4/ip_forward
+fi
+
+pwd_dir=`pwd`
+config_file="${pwd_dir}/alertmanager.yml"
+
+docker pull quay.io/prometheus/alertmanager
+docker run -d --name alertmanager -p 9093:9093 \
+ -v /$pwd_dir:/alertmanager-conf \
+ quay.io/prometheus/alertmanager \
+ -config.file=/alertmanager-conf/alertmanager.yml 
