1bdadc07345f2728fb417c13f29d39a3550491b2
zhangdongqi <zhangdongqi@qiniu.com>
2017-07-24 20:14:39 +0800

    node-exporter can not run on mac

--- deploy/node_exporter/docker_deploy.sh
+++ deploy/node_exporter/docker_deploy.sh
@@ -1,13 +1,9 @@
 #! /bin/sh
 os=`uname -s`
-if test $os = "Darwin" ; then
-    echo "Darwin"
-    sysctl -w net.inet.ip.forwarding=1
-fi
 
-if test $os = "Linux" ; then
-    echo "Linux"
-    echo "1" > /proc/sys/net/ipv4/ip_forward
+if test $os != "Linux" ; then
+    echo "Not linux system, can not run node-exporter."
+    exit 0
 fi
 
 docker run -d --name node-exporter -p 9100:9100 \
