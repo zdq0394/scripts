4b32115713da854123f7e88a2106d727dc88ed0b
zhangdongqi <zhangdongqi@qiniu.com>
2017-07-24 17:26:04 +0800

    etcd name

--- deploy/etcd/deploy_static.sh
+++ deploy/etcd/deploy_static.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 # Config etcd nodes
+ETCD_NAME_1=""
 CLUSTER_IP_1=""
+ETCD_NAME_2=""
 CLUSTER_IP_2=""
+ETCD_NAME_3=""
 CLUSTER_IP_3=""
+
+ETCD_NAME=$ETCD_NAME_1
 HOST_IP=$CLUSTER_IP_1
 
 yum install -y etcd
 
-tee /etc/etcd/etcd.conf <<-'EOF'
-ETCD_NAME=etcd03
-ETCD_DATA_DIR="/var/lib/etcd/etcd03"
+cat <<EOF > /etc/etcd/etcd.conf
+ETCD_NAME=$ETCD_NAME
+ETCD_DATA_DIR="/var/lib/etcd/$ETCD_NAME_1"
 ETCD_LISTEN_PEER_URLS="http://$HOST_IP:2380"
 ETCD_LISTEN_CLIENT_URLS="http://$HOST_IP:2379,http://127.0.0.1:2379"
 ETCD_INITIAL_ADVERTISE_PEER_URLS="http://$HOST_IP:2380"
 ETCD_ADVERTISE_CLIENT_URLS="http://$HOST_IP:2379"
 ETCD_INITIAL_CLUSTER_STATE="new"
 ETCD_INITIAL_CLUSTER_TOKEN="etcd-cluster1"
-ETCD_INITIAL_CLUSTER="etcd01=http://$CLUSTER_IP_1:2380,etcd02=http://$CLUSTER_IP_2:2380,etcd03=http://$CLUSTER_IP_3:2380"
+ETCD_INITIAL_CLUSTER="$ETCD_NAME_1=http://$CLUSTER_IP_1:2380,$ETCD_NAME_2=http://$CLUSTER_IP_2:2380,$ETCD_NAME_3=http://$CLUSTER_IP_3:2380"
 EOF
 
 tee  /usr/lib/systemd/system/etcd.service <<-'EOF'
