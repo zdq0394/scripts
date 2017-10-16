8b10d55a7215c0f71513f531efc4cbb3eba58a46
dongqi zhang <zdq123.hn@163.com>
2017-10-16 21:25:11 +0800

    export mongo port

--- deploy/mongodb/deploy_under_docker.sh
+++ deploy/mongodb/deploy_under_docker.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 
-mkdir -p /data/mongo_data
+mkdir -p mongo_data
 
 docker run --name mongo0 \
- -v /data/mongo_data:/data/db \
+ -v mongo_data:/data/db \
+ -p 27017:27017 \
  -d mongo:3.2.16
