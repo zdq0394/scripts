7043da5fc91e438aa45f3b6f44120e4b8bf66b62
zdq0394 <zdq123.hn@163.com>
2017-11-29 18:43:58 +0800

    Ceph install/uninstall

---
+++ iaas/ceph/README.md
@@ -0,0 +1,21 @@
+Usage
+Before install ceph ,make sure your VM or Server can connet to Internet.
+The install.sh script will install a HAMMER ceph cluster with ONLY ONE OSD under/root/osd, this is used for quick installation in a clean envonriment.
+If you have a pre-installed newer version ceph, you need to uninstall it. See the Unistall.
+Install
+git clone https://github.com/zdq0394/scripts.git
+cd scripts/iass/ceph
+chmod +x *.sh
+./install.sh                                                                                                                                                
+
+Clean up and Uninstall
+This script will clean up your ceph environment without uninstall ceph packages. This is very helpful when you get some problems when install ceph especially when you have an older unclean environmet, it is always recommnented to Clean Up your env before install ceph.
+
+Clean up
+./cleanup.sh
+Uninstall
+When you want to install ceph from newer version to an older version, e.g. 10.2.3 to 0.94.9, you need to uninstall the 10.2.3 ceph packages, and then install the 0.94.9 version. This is quite simple:
+# ceph -v
+ceph version 10.2.2 (45107e21c568dd033c2f0a3107dec8f0b0e58374)
+
+# rpm -qa | grep 10.2.2 | xargs -e --nodeps
---
+++ iaas/ceph/install.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+echo "nameserver 114.114.114.114" > /etc/resolv.conf
+
+IP=`ip route get 114.114.114.114 | awk 'NR==1 {print $NF}'`
+
+echo $IP $HOSTNAME >>  /etc/hosts
+
+
+
+
+
+yum clean all
+
+rm -rf /etc/yum.repos.d/*.repo
+
+wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+
+wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo
+
+sed -i '/aliyuncs/d' /etc/yum.repos.d/CentOS-Base.repo
+
+sed -i 's/$releasever/7/g' /etc/yum.repos.d/CentOS-Base.repo
+
+sed -i '/aliyuncs/d' /etc/yum.repos.d/epel.repo
+
+
+
+echo "
+
+[ceph]
+
+name=ceph
+
+baseurl=http://mirrors.aliyun.com/ceph/rpm-hammer/el7/x86_64/
+
+gpgcheck=0
+
+[ceph-noarch]
+
+name=cephnoarch
+
+baseurl=http://mirrors.aliyun.com/ceph/rpm-hammer/el7/noarch/
+
+gpgcheck=0
+
+" > /etc/yum.repos.d/ceph.repo
+
+
+
+yum install ceph ceph-radosgw ceph-deploy -y
+
+
+
+mkdir -p  /root/cluster & cd /root/cluster & rm -f /root/cluster/*
+
+cd /root/cluster
+
+
+
+sed -i 's/SELINUX=.*/SELINUX=disabled/' /etc/selinux/config
+
+setenforce 0
+
+systemctl stop firewalld 
+
+systemctl disable firewalld
+
+
+
+ceph-deploy new $HOSTNAME
+
+
+
+echo osd pool default size = 1 >> ceph.conf
+
+echo osd crush chooseleaf type = 0 >> ceph.conf
+
+echo osd max object name len = 256 >> ceph.conf
+
+echo osd journal size = 128 >> ceph.conf
+
+
+
+mkdir -p /var/run/ceph/
+
+chown ceph:ceph /var/run/ceph/
+
+mkdir -p /osd & rm -rf /osd/*
+
+chown ceph:ceph /osd
+
+ceph-deploy mon create-initial
+
+ceph-deploy osd prepare $HOSTNAME:/osd
+
+ceph-deploy osd activate  $HOSTNAME:/osd
+
+
+
+ceph -s
