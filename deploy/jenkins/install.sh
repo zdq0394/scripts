eb56854eecd99bf7fcfc0ae553a15006a95a48a1
dongqi zhang <zdq123.hn@163.com>
2017-12-04 11:37:37 +0800

    install jenkins.sh

---
+++ deploy/jenkins/install.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+# Install Java
+echo "Install Java"
+apt-get install -y openjdk-8-jre-headless
+# Install Jenkins
+JENKINS_ROOT=/var/lib/jenkins
+JENKINS_HOME=$JENKINS_ROOT/jenkins_home
+JENKINS_NODE=$JENKINS_ROOT/jenkins_node
+rm -fr $JENKINS_ROOT
+mkdir -p $JENKINS_ROOT
+mkdir -p $JENKINS_HOME
+mkdir -p $JENKINS_NODE
+cd $JENKINS_ROOT
+wget http://updates.jenkins-ci.org/download/war/2.73.3/jenkins.war
+
+export JENKINS_HOME
+nohup java -jar $JENKINS_ROOT/jenkins.war --httpPort=8080 > jenkins.out 2>&1 &
