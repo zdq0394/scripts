9a6fca55767639a2876f8a7a88d33e39217b4833
zdq0394 <zdq123.hn@163.com>
2018-05-10 13:10:12 +0800

    deploy in k8s

--- deploy/prometheus/deploy_in_k8s/deploy.sh
+++ deploy/prometheus/deploy_in_k8s/deploy.sh
@@ -5,6 +5,8 @@ kubectl apply -f namespace.yaml
 kubectl apply -f alertmanager/alertmanager.cm.yaml
 kubectl apply -f alertmanager/alertmanager.deployment.yaml
 kubectl apply -f alertmanager/alertmanager.svc.yaml
+# Create node exporter
+kubectl apply -f exporter/node_exporter.daemonset.yaml
 # Create prometheus-server
 kubectl apply -f prometheus/prometheus-rules.cm.yaml
 kubectl apply -f prometheus/prometheus.cm.yaml
@@ -12,5 +14,8 @@ kubectl apply -f prometheus/prometheus.deployment.yaml
 kubectl apply -f prometheus/prometheus.svc.yaml
 # Create grafana
 kubectl apply -f grafana/grafana.ds.cm.yaml
+kubectl apply -f grafana/grafana.dashboard.k8s-cluster-health.yaml
+kubectl apply -f grafana/grafana.dashboard.k8s-cluster-monitoring.yaml
+kubectl apply -f grafana/grafana.dashboard.cm.yaml
 kubectl apply -f grafana/grafana.deployment.yaml
 kubectl apply -f grafana/grafana.svc.yaml
