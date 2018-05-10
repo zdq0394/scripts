45ed8fc7a4de31ca160dacad92c85f312d10ea52
zdq0394 <zdq123.hn@163.com>
2018-05-10 18:50:00 +0800

    prometheus add node-exporter grafana ui

--- deploy/prometheus/deploy_in_k8s/deploy.sh
+++ deploy/prometheus/deploy_in_k8s/deploy.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 # Create namespace monitoring
 kubectl apply -f namespace.yaml
+
+# Create node exporter
+kubectl apply -f exporter/node_exporter.daemonset.yaml
+
 # Create alertmanager
 kubectl apply -f alertmanager/alertmanager.cm.yaml
 kubectl apply -f alertmanager/alertmanager.deployment.yaml
 kubectl apply -f alertmanager/alertmanager.svc.yaml
-# Create node exporter
-kubectl apply -f exporter/node_exporter.daemonset.yaml
+
+# sleep a while in seconds
+sleep 15
 # Create prometheus-server
 kubectl apply -f prometheus/prometheus-rules.cm.yaml
 kubectl apply -f prometheus/prometheus.cm.yaml
@@ -16,6 +21,7 @@ kubectl apply -f prometheus/prometheus.svc.yaml
 kubectl apply -f grafana/grafana.ds.cm.yaml
 kubectl apply -f grafana/grafana.dashboard.k8s-cluster-health.yaml
 kubectl apply -f grafana/grafana.dashboard.k8s-cluster-monitoring.yaml
+kubectl apply -f grafana/grafana.dashboard.docker-host.yaml
 kubectl apply -f grafana/grafana.dashboard.cm.yaml
 kubectl apply -f grafana/grafana.deployment.yaml
 kubectl apply -f grafana/grafana.svc.yaml
