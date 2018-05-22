a2173d37df5eea86d605deb82680b03ef732de53
zdq0394 <zdq123.hn@163.com>
2018-05-22 18:54:17 +0800

    update

--- deploy/prometheus/deploy_in_k8s/deploy.sh
+++ deploy/prometheus/deploy_in_k8s/deploy.sh
@@ -4,6 +4,15 @@ kubectl apply -f namespace.yaml
 
 # Create node exporter
 kubectl apply -f exporter/node_exporter.daemonset.yaml
+kubectl apply -f exporter/node_exporter.svc.yaml
+
+# Create SMS hook
+kubectl apply -f smshook/smshook.cm.yaml
+kubectl apply -f smshook/smshook.deployment.yaml
+kubectl apply -f smshook/smshook.svc.yaml
+
+# sleep a while in seconds
+sleep 15s
 
 # Create alertmanager
 kubectl apply -f alertmanager/alertmanager.cm.yaml
@@ -13,7 +22,14 @@ kubectl apply -f alertmanager/alertmanager.svc.yaml
 # sleep a while in seconds
 sleep 15
 # Create prometheus-server
+## Create prometheus server alert rules
 kubectl apply -f prometheus/prometheus-rules.cm.yaml
+kubectl apply -f prometheus/rules/target.rules.yaml
+kubectl apply -f prometheus/rules/node.rules.yaml
+kubectl apply -f prometheus/rules/etcd.rules.yaml
+kubectl apply -f prometheus/rules/apiserver.rules.yaml
+kubectl apply -f prometheus/rules/pod.rules.yaml
+## Create prometheus server
 kubectl apply -f prometheus/prometheus.cm.yaml
 kubectl apply -f prometheus/prometheus.deployment.yaml
 kubectl apply -f prometheus/prometheus.svc.yaml
