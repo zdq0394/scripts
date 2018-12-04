82850d0b875661ae3268f0477f9e3d605cec6023
zdq0394 <zdq123.hn@163.com>
2018-12-04 15:24:10 +0800

    redis cluster deploy bug fix

--- deploy/redis/cluster/cluster.sh
+++ deploy/redis/cluster/cluster.sh
@@ -1,5 +1,9 @@
 #! /bin/sh
 REDIS_IMAGE=registry.docker-cn.com/library/redis:5.0.1
-docker run -d --name=redis0 --hostname=redis0 -v /redis-conf:/etc/redis $REDIS_IMAGE redis-server /etc/redis/redis0.conf 
-docker run -d --name=redis1 --hostname=redis1 -v /redis-conf:/etc/redis $REDIS_IMAGE redis-server /etc/redis/redis1.conf 
-docker run -d --name=redis2 --hostname=redis2 -v /redis-conf:/etc/redis $REDIS_IMAGE redis-server /etc/redis/redis2.conf
+CONF_OPTIONS="-v /opt/gopath/src/github.com/zdq0394/scripts/deploy/redis/cluster/redis-conf:/etc/redis"
+docker run -d --name=redis0 --hostname=redis0 $CONF_OPTIONS $REDIS_IMAGE redis-server /etc/redis/redis0.conf 
+docker run -d --name=redis1 --hostname=redis1 $CONF_OPTIONS $REDIS_IMAGE redis-server /etc/redis/redis1.conf 
+docker run -d --name=redis2 --hostname=redis2 $CONF_OPTIONS $REDIS_IMAGE redis-server /etc/redis/redis2.conf
+docker run -d --name=redis3 --hostname=redis3 $CONF_OPTIONS $REDIS_IMAGE redis-server /etc/redis/redis3.conf 
+docker run -d --name=redis4 --hostname=redis4 $CONF_OPTIONS $REDIS_IMAGE redis-server /etc/redis/redis4.conf 
+docker run -d --name=redis5 --hostname=redis5 $CONF_OPTIONS $REDIS_IMAGE redis-server /etc/redis/redis5.conf
