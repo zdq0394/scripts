2b78f79bbe6607bc9f781ca03375e66f7fa96e90
zdq0394 <zdq123.hn@163.com>
2018-12-18 21:59:36 +0800

    redis cluster adjust

--- deploy/redis/cluster/docker/cluster.sh
+++ deploy/redis/cluster/docker/cluster.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 echo "Start 6 redis nodes..."
 REDIS_IMAGE=registry.docker-cn.com/library/redis:5.0.1
-CONF_OPTIONS="-v /opt/gopath/src/github.com/zdq0394/scripts/deploy/redis/cluster/redis-conf:/etc/redis"
+CONF_OPTIONS="-v /opt/gopath/src/github.com/zdq0394/scripts/deploy/redis/cluster/docker/redis-conf:/etc/redis"
 docker run -d --name=redis0 --hostname=redis0 $CONF_OPTIONS $REDIS_IMAGE redis-server /etc/redis/redis0.conf 
 docker run -d --name=redis1 --hostname=redis1 $CONF_OPTIONS $REDIS_IMAGE redis-server /etc/redis/redis1.conf 
 docker run -d --name=redis2 --hostname=redis2 $CONF_OPTIONS $REDIS_IMAGE redis-server /etc/redis/redis2.conf
