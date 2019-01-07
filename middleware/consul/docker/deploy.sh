ef6dc88319589ac58466f42c1a649a60ddb57e5f
zdq0394 <zdq123.hn@163.com>
2019-01-07 18:07:56 +0800

    add diff ports

--- middleware/consul/docker/deploy.sh
+++ middleware/consul/docker/deploy.sh
@@ -9,13 +9,13 @@ docker run -d --name=consul0 --hostname=consul0 $CONF0_OPTIONS $CONSUL_IMAGE \
     -node=consul0 -data-dir=/tmp/consul -config-dir=/etc/consul.d \
     -enable-script-checks=true -ui=true
 
-CONF1_OPTIONS="-v $WORKDIR/consul1:/etc/consul.d"
+CONF1_OPTIONS="-p 8501:8500 -v $WORKDIR/consul1:/etc/consul.d"
 docker run -d --name=consul1 --hostname=consul1 $CONF1_OPTIONS $CONSUL_IMAGE \
     agent -server -bind=0.0.0.0 -client=0.0.0.0 \
     -node=consul1 -data-dir=/tmp/consul -config-dir=/etc/consul.d \
     -enable-script-checks=true -ui=true
 
-CONF2_OPTIONS="-v $WORKDIR/consul2:/etc/consul.d"
+CONF2_OPTIONS="-p 8502:8500 -v $WORKDIR/consul2:/etc/consul.d"
 docker run -d --name=consul2 --hostname=consul2 $CONF2_OPTIONS $CONSUL_IMAGE \
     agent -server -bind=0.0.0.0 -client=0.0.0.0 \
     -node=consul2 -data-dir=/tmp/consul -config-dir=/etc/consul.d \
