a80de443f2741097f79a6b492ef243d924eb8244
zdq0394 <zdq123.hn@163.com>
2019-01-07 17:10:59 +0800

    consul enable ui

--- middleware/consul/docker/deploy.sh
+++ middleware/consul/docker/deploy.sh
@@ -5,21 +5,23 @@ WORKDIR=`pwd`
 
 CONF0_OPTIONS="-p 8500:8500 -v $WORKDIR/consul0:/etc/consul.d"
 docker run -d --name=consul0 --hostname=consul0 $CONF0_OPTIONS $CONSUL_IMAGE \
-    agent -server -bind=0.0.0.0 -bootstrap-expect=3 \
+    agent -server -bind=0.0.0.0 -client=0.0.0.0 -bootstrap-expect=3 \
     -node=consul0 -data-dir=/tmp/consul -config-dir=/etc/consul.d \
-    -enable-script-checks=true
+    -enable-script-checks=true -ui=true
 
 CONF1_OPTIONS="-v $WORKDIR/consul1:/etc/consul.d"
 docker run -d --name=consul1 --hostname=consul1 $CONF1_OPTIONS $CONSUL_IMAGE \
-    agent -server -bind=0.0.0.0 \
+    agent -server -bind=0.0.0.0 -client=0.0.0.0 \
     -node=consul1 -data-dir=/tmp/consul -config-dir=/etc/consul.d \
-    -enable-script-checks=true
+    -enable-script-checks=true -ui=true
 
 CONF2_OPTIONS="-v $WORKDIR/consul2:/etc/consul.d"
 docker run -d --name=consul2 --hostname=consul2 $CONF2_OPTIONS $CONSUL_IMAGE \
-    agent -server -bind=0.0.0.0 \
+    agent -server -bind=0.0.0.0 -client=0.0.0.0 \
     -node=consul2 -data-dir=/tmp/consul -config-dir=/etc/consul.d \
-    -enable-script-checks=true
+    -enable-script-checks=true -ui=true
 
 docker exec -it consul1 consul join 172.17.0.2
 docker exec -it consul2 consul join 172.17.0.2
+
+docker exec -it consul0 consul members
