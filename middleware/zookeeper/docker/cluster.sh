ca9118b6cb871acdb1673af4fdd777db1e02ca37
zdq0394 <zdq123.hn@163.com>
2019-01-18 13:29:29 +0800

    zk cluster

--- middleware/zookeeper/docker/cluster.sh
+++ middleware/zookeeper/docker/cluster.sh
@@ -1,12 +1,10 @@
 #! /bin/sh
 ZK_IMG="zookeeper:3.4.13"
+
 ### zk1
 node=zk1
 rm -fr /tmp/$node
-mkdir -p /tmp/$node/conf
-mkdir -p /tmp/$node/data
-mkdir -p /tmp/$node/datalog
-mkdir -p /tmp/$node/logs
+cp -R $node /tmp/$node
 VOLUME_OPTIONS="-v /tmp/$node/conf:/conf -v /tmp/$node/data:/data -v /tmp/$node/datalog:/datalog -v /tmp/$node/logs:/logs"
 
 docker run -d --name=$node --hostname=$node $VOLUME_OPTIONS $ZK_IMG
@@ -14,10 +12,7 @@ docker run -d --name=$node --hostname=$node $VOLUME_OPTIONS $ZK_IMG
 ### zk2
 node=zk2
 rm -fr /tmp/$node
-mkdir -p /tmp/$node/conf
-mkdir -p /tmp/$node/data
-mkdir -p /tmp/$node/datalog
-mkdir -p /tmp/$node/logs
+cp -R $node /tmp/$node
 VOLUME_OPTIONS="-v /tmp/$node/conf:/conf -v /tmp/$node/data:/data -v /tmp/$node/datalog:/datalog -v /tmp/$node/logs:/logs"
 
 docker run -d --name=$node --hostname=$node $VOLUME_OPTIONS $ZK_IMG
@@ -25,10 +20,7 @@ docker run -d --name=$node --hostname=$node $VOLUME_OPTIONS $ZK_IMG
 ### zk3
 node=zk3
 rm -fr /tmp/$node
-mkdir -p /tmp/$node/conf
-mkdir -p /tmp/$node/data
-mkdir -p /tmp/$node/datalog
-mkdir -p /tmp/$node/logs
+cp -R $node /tmp/$node
 VOLUME_OPTIONS="-v /tmp/$node/conf:/conf -v /tmp/$node/data:/data -v /tmp/$node/datalog:/datalog -v /tmp/$node/logs:/logs"
 
 docker run -d --name=$node --hostname=$node $VOLUME_OPTIONS $ZK_IMG
