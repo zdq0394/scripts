123407956a71b5df1d893e403d3bf473764dece3
zdq0394 <zdq123.hn@163.com>
2019-01-18 13:18:02 +0800

    add zk1/zk2/zk3

--- middleware/zookeeper/docker/cluster.sh
+++ middleware/zookeeper/docker/cluster.sh
@@ -1,10 +1,34 @@
 #! /bin/sh
 ZK_IMG="zookeeper:3.4.13"
+### zk1
+node=zk1
+rm -fr /tmp/$node
+mkdir -p /tmp/$node/conf
+mkdir -p /tmp/$node/data
+mkdir -p /tmp/$node/datalog
+mkdir -p /tmp/$node/logs
+VOLUME_OPTIONS="-v /tmp/$node/conf:/conf -v /tmp/$node/data:/data -v /tmp/$node/datalog:/datalog -v /tmp/$node/logs:/logs"
 
-mkdir -p /tmp/zk1/conf
-mkdir -p /tmp/zk1/data
-mkdir -p /tmp/zk1/datalog
-mkdir -p /tmp/zk1/logs
-VOLUME_OPTIONS1="-v /tmp/zk1/conf:/conf -v /tmp/zk1/data:/data -v /tmp/zk1/datalog:/datalog -v /tmp/zk1/logs:/logs"
+docker run -d --name=$node --hostname=$node $VOLUME_OPTIONS $ZK_IMG
 
-docker run -d --name=zk1 $VOLUME_OPTIONS1 $ZK_IMG
+### zk2
+node=zk2
+rm -fr /tmp/$node
+mkdir -p /tmp/$node/conf
+mkdir -p /tmp/$node/data
+mkdir -p /tmp/$node/datalog
+mkdir -p /tmp/$node/logs
+VOLUME_OPTIONS="-v /tmp/$node/conf:/conf -v /tmp/$node/data:/data -v /tmp/$node/datalog:/datalog -v /tmp/$node/logs:/logs"
+
+docker run -d --name=$node --hostname=$node $VOLUME_OPTIONS $ZK_IMG
+
+### zk3
+node=zk3
+rm -fr /tmp/$node
+mkdir -p /tmp/$node/conf
+mkdir -p /tmp/$node/data
+mkdir -p /tmp/$node/datalog
+mkdir -p /tmp/$node/logs
+VOLUME_OPTIONS="-v /tmp/$node/conf:/conf -v /tmp/$node/data:/data -v /tmp/$node/datalog:/datalog -v /tmp/$node/logs:/logs"
+
+docker run -d --name=$node --hostname=$node $VOLUME_OPTIONS $ZK_IMG
