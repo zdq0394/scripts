d011500704af2feabe77c29522421b2ee2d0f695
zdq0394 <zdq123.hn@163.com>
2019-01-18 11:10:54 +0800

    bugfix

--- middleware/zookeeper/docker/cluster.sh
+++ middleware/zookeeper/docker/cluster.sh
@@ -3,6 +3,6 @@ ZK_IMG="zookeeper:3.4.13"
 
 PWD_DIR=`pwd`
 
-VOLUME_OPTIONS1="-v $PWD_DIR/zk1/conf:/conf -v $PWD_DIR/zk1/data:/data -v $PWD_DIR/zk1/logs:/datalog"
+VOLUME_OPTIONS1="-v $PWD_DIR/zk1/conf:/conf -v $PWD_DIR/zk1/data:/data -v $PWD_DIR/zk1/datalog:/datalog -v $PWD_DIR/zk1/logs:/logs"
 
 docker run -d --name=zk1 $VOLUME_OPTIONS1 $ZK_IMG
