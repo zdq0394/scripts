664062585ae77e549dea33c6c35192e1e281ba5c
zdq0394 <zdq123.hn@163.com>
2019-01-18 11:29:21 +0800

    zookeeper

--- middleware/zookeeper/docker/cluster.sh
+++ middleware/zookeeper/docker/cluster.sh
@@ -1,8 +1,10 @@
 #! /bin/sh
 ZK_IMG="zookeeper:3.4.13"
 
-PWD_DIR=`pwd`
-
-VOLUME_OPTIONS1="-v $PWD_DIR/zk1/conf:/conf -v $PWD_DIR/zk1/data:/data -v $PWD_DIR/zk1/datalog:/datalog -v $PWD_DIR/zk1/logs:/logs"
+mkdir -p /tmp/zk1/conf
+mkdir -p /tmp/zk1/data
+mkdir -p /tmp/zk1/datalog
+mkdir -p /tmp/zk1/logs
+VOLUME_OPTIONS1="-v /tmp/zk1/conf:/conf -v /tmp/zk1/data:/data -v /tmp/zk1/datalog:/datalog -v /tmp/zk1/logs:/logs"
 
 docker run -d --name=zk1 $VOLUME_OPTIONS1 $ZK_IMG
