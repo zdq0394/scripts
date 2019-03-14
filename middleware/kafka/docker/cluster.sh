40911e6f479f6ae790d8fd1042bc2c59e03da93d
zdq0394 <zdq123.hn@163.com>
2019-03-14 14:03:39 +0800

    kafka docker

--- middleware/kafka/docker/cluster.sh
+++ middleware/kafka/docker/cluster.sh
@@ -5,10 +5,13 @@ ZK_SERVERS="172.17.0.2:2181,172.17.0.3:2181,172.17.0.4:2181"
 # kafka-0
 ID="0"
 KAFKA_NAME="kafka-$ID"
-docker run -d --name=$KAFKA_NAME --hostname=$KAFKA_NAME \
+LOGS_DIR="/tmp/kafka$ID"
+mkdir -p $LOGS_DIR
+docker run -d --name=$KAFKA_NAME --hostname=$KAFKA_NAME -v $LOGS_DIR:/var/log/kafka \
   --env "KAFKA_BROKER_ID"="$ID" \
   --env "KAFKA_ZOOKEEPER_CONNECT"="$ZK_SERVERS" \
   --env "KAFKA_LISTENERS"="PLAINTEXT://:9092" \
+  --env "KAFKA_LOG_DIRS"="/var/log/kafka" \
   --env "KAFKA_HEAP_OPTS"="-Xmx6g -Xms1g" \
   --env "KAFKA_NUM_IO_THREADS"="2" \
   $KAFKA_IMG
@@ -16,10 +19,13 @@ docker run -d --name=$KAFKA_NAME --hostname=$KAFKA_NAME \
 # kafka-1
 ID="1"
 KAFKA_NAME="kafka-$ID"
-docker run -d --name=$KAFKA_NAME --hostname=$KAFKA_NAME \
+LOGS_DIR="/tmp/kafka$ID"
+mkdir -p $LOGS_DIR
+docker run -d --name=$KAFKA_NAME --hostname=$KAFKA_NAME -v $LOGS_DIR:/var/log/kafka \
   --env "KAFKA_BROKER_ID"="$ID" \
   --env "KAFKA_ZOOKEEPER_CONNECT"="$ZK_SERVERS" \
   --env "KAFKA_LISTENERS"="PLAINTEXT://:9092" \
+  --env "KAFKA_LOG_DIRS"="/var/log/kafka" \
   --env "KAFKA_HEAP_OPTS"="-Xmx6g -Xms1g" \
   --env "KAFKA_NUM_IO_THREADS"="2" \
   $KAFKA_IMG
@@ -27,10 +33,13 @@ docker run -d --name=$KAFKA_NAME --hostname=$KAFKA_NAME \
 # kafka-2
 ID="2"
 KAFKA_NAME="kafka-$ID"
-docker run -d --name=$KAFKA_NAME --hostname=$KAFKA_NAME \
+LOGS_DIR="/tmp/kafka$ID"
+mkdir -p $LOGS_DIR
+docker run -d --name=$KAFKA_NAME --hostname=$KAFKA_NAME -v $LOGS_DIR:/var/log/kafka \
   --env "KAFKA_BROKER_ID"="$ID" \
   --env "KAFKA_ZOOKEEPER_CONNECT"="$ZK_SERVERS" \
   --env "KAFKA_LISTENERS"="PLAINTEXT://:9092" \
+  --env "KAFKA_LOG_DIRS"="/var/log/kafka" \
   --env "KAFKA_HEAP_OPTS"="-Xmx6g -Xms1g" \
   --env "KAFKA_NUM_IO_THREADS"="2" \
   $KAFKA_IMG
