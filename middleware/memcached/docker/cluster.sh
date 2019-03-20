9708b7d044c25cb82f8fd9b7c1762e4d5e594a07
zdq0394 <zdq123.hn@163.com>
2019-03-20 13:30:05 +0800

    memcached cluster 3 nodes

--- middleware/memcached/docker/cluster.sh
+++ middleware/memcached/docker/cluster.sh
@@ -1,4 +1,8 @@
 #! /bin/sh
 IMG=memcached:1.5.12
 
-docker run --name memcached-0 -p 11211:11211 -d $IMG
+docker run --name memcached-0 -p 21210:11211 -d $IMG
+
+docker run --name memcached-1 -p 21211:11211 -d $IMG
+
+docker run --name memcached-2 -p 21212:11211 -d $IMG
