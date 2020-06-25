9a15ea22634dc3eaec37758046408d52c62475ac
zhangdongqi <zdq123.hn@163.com>
2020-06-25 14:19:53 +0800

    fix es

--- infrastructure/elk/docker/cluster-all-in-one.sh
+++ infrastructure/elk/docker/cluster-all-in-one.sh
@@ -3,11 +3,15 @@ mkdir -p /elastic/data/es01
 mkdir -p /elastic/data/es02
 mkdir -p /elastic/data/es03
 
-chmod -R 777 /elastic/data
+chmod -R g+rwx /elastic/data
+chgrp -R 0 /elastic/data
+
+sysctl -w vm.max_map_count=262144
 
 docker network create elastic
 
-docker run -d --name es01 --hostname es01\
+docker run -d --name es01 --hostname es01 \
+  --ulimit nofile=65535:65535 \
   --net "elastic" \
   -p 9200:9200 \
   -e "node.name=es01" \
@@ -19,7 +23,8 @@ docker run -d --name es01 --hostname es01\
   -v /elastic/data/es01:/usr/share/elasticsearch/data:rw \
   -it elastic/elasticsearch:7.8.0
 
-docker run -d --name es02 --hostname es02\
+docker run -d --name es02 --hostname es02 \
+  --ulimit nofile=65535:65535 \
   --net "elastic" \
   -e "node.name=es02" \
   -e "cluster.name=es-docker-cluster" \
@@ -31,6 +36,7 @@ docker run -d --name es02 --hostname es02\
   -it elastic/elasticsearch:7.8.0
 
 docker run -d --name es03 --hostname es03\
+  --ulimit nofile=65535:65535 \
   --net "elastic" \
   -e "node.name=es03" \
   -e "cluster.name=es-docker-cluster" \
