b3b462ffe25abd31730d66aa37bf04e2394fe7aa
zhangdongqi <zdq123.hn@163.com>
2020-06-25 15:55:56 +0800

    remove metricbeat

--- infrastructure/elk/docker/metric-beat.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-docker run -d --name metricbeat \
-    --net "elastic" \
-    docker.elastic.co/beats/metricbeat:7.8.0 \
-    setup -E setup.kibana.host=kibana:5601 \
-    -E output.elasticsearch.hosts=["es01:9200"]
